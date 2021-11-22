7c6b1f1edca558faad510122eca96b10308945d4
Mrudang Shah <mrudang@geeksinvention.com>
2021-11-22 14:48:47 +0530

    Intial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { orderDetails } from '../data';
+import { EditService, ToolbarService, PageService } from '@syncfusion/ej2-angular-grids';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    providers: [ToolbarService, EditService, PageService]
+})
+export class AppComponent {
+    public data!: Object[];
+    public editSettings!: Object;
+    public toolbar!: string[];
+    public orderidrules!: Object;
+    public customeridrules!: Object;
+    public freightrules!: Object;
+    public pageSettings!: Object;
+    public editparams!: Object;
+
+    public ngOnInit(): void {
+        this.data = orderDetails;
+        this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Dialog' };
+        this.toolbar = ['Add', 'Edit', 'Delete'];
+        this.orderidrules = { required: true, number: true };
+        this.customeridrules = { required: true };
+        this.freightrules =  { required: true };
+        this.editparams = { params: { popupHeight: '300px' }};
+        this.pageSettings = { pageCount: 5};
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { CheckBoxModule } from '@syncfusion/ej2-angular-buttons';
+import { DropDownListAllModule } from '@syncfusion/ej2-angular-dropdowns';
+import { DatePickerAllModule } from '@syncfusion/ej2-angular-calendars';
+import { ToolbarModule } from '@syncfusion/ej2-angular-navigations';
+import { NumericTextBoxAllModule } from '@syncfusion/ej2-angular-inputs';
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+import { GridAllModule } from '@syncfusion/ej2-angular-grids';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CheckBoxModule,
+    DropDownListAllModule,
+    DatePickerAllModule,
+    ToolbarModule,
+    NumericTextBoxAllModule,
+    DialogModule,
+    GridAllModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
